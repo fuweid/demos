5d43b4a72e7b3991ce66845803fc423b6e9d7985
Wei Fu <fuweid89@gmail.com>
2022-02-27 19:20:47 +0800

    ebpf: init example-field-offset-rewrite example
    Signed-off-by: Wei Fu <fuweid89@gmail.com>

---
+++ ebpf/example-field-offset-rewrite.bpf.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+SEC("tp/sched/sched_process_exec")
+int handle_exec(struct trace_event_raw_sched_process_exec *ctx)
+{
+	struct task_struct *task;
+	pid_t pid;
+
+	task = (struct task_struct *)bpf_get_current_task();
+	pid = BPF_CORE_READ(task, real_parent, pid);
+	return pid;
+}
